2cf17520b17fa0d39cb40a91a5dc9f7af91f24d5
gurleen.singh <gurleen.singh@B108-18.soit.local>
2015-07-31 09:16:31 +1200

    Learntostartv1

---
+++ src/learntostart/Learntostart.java
@@ -0,0 +1,17 @@
+
+package learntostart;
+
+/**
+ *
+ * @author gurleen.singh
+ */
+public class Learntostart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is 1st Person");
+    }
+    
+}
